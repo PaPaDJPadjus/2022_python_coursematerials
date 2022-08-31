7adef4d17f8ed52b2df35be7e74df1836aa61e8e
Patrick Padjus <papadj@ttu.ee>
2022-08-31 11:13:47 +0300

    asd

---
+++ EX/ex01_hello/hello.py
@@ -0,0 +1,7 @@
+"""Hello code."""
+name = input("What is your name? ")   # asks for name
+esimene = int(input("Hello, " + str(name) + "! Enter a random number: "))   # says name and asks for first number
+print(esimene)
+teine = int(input("Great! Now enter a second random number: "))    # asks for second number
+print(teine)
+print(str(esimene) + " + " + str(teine) + " " + "is " + str(esimene + teine))    # prints equation with answer
