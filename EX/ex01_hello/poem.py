e36e90fcab6b25ab7f417e9ae4a2f43926a7dae7
Patrick Padjus <papadj@ttu.ee>
2022-08-31 11:32:51 +0300

    asd

---
+++ EX/ex01_hello/poem.py
@@ -0,0 +1,5 @@
+colour = input("Enter a colour ")
+object = input("Enter plural objects ")
+activity = input("Enter an activity ")
+
+print(f"Roses are {colour}, \n{object} are blue, \nI love to {activity} \nAnd so will you!")
