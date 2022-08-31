62fa9c30f8f258a06893eb3394710f873e53fafb
Patrick Padjus <papadj@ttu.ee>
2022-08-31 17:16:51 +0300

    3. osa

---
+++ EX/ex01_hello/greetings.py
@@ -0,0 +1,5 @@
+greeting = input("Enter a greeting: ")
+recipient = input("Enter a recipient: ")
+times = int(input("How many times to repeat: "))
+full = greeting + " " + recipient + "! "
+print(full * times)
