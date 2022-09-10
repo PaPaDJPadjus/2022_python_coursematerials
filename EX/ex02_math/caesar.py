e97be1a94ec848bd2f3381a93a9e13a26e239c62
Patrick Padjus <papadj@ttu.ee>
2022-09-10 20:50:19 +0300

    Prime nrs

---
+++ EX/ex02_math/caesar.py
@@ -0,0 +1,47 @@
+"""Caesar cipher."""
+
+
+def encode(message: str, shift: int):
+    """
+    Encode a message using a Caesar cipher.
+
+    Presume the message is already lowercase.
+    For each letter of the message, shift it forward in the alphabet by shift amount.
+    If the character isn't a letter, keep it the same.
+
+    For example, shift = 3 then a => d, b => e, z => c (see explanation below)
+
+    Shift:    0 1 2 3
+    Alphabet:       A B C D E F G H I J
+    Result:   A B C D E F G H I J
+
+    Examples:
+    1. encode('i like turtles', 6) == 'o roqk zaxzrky'
+    2. encode('example', 1) == 'fybnqmf'
+    3. encode('the quick brown fox jumps over the lazy dog.', 7) == 'aol xbpjr iyvdu mve qbtwz vcly aol shgf kvn.'
+
+    :param message: message to be encoded
+    :param shift: shift for encoding
+    :return: encoded message
+    """
+    encoded_message = ""
+    alphabet = "abcdefghijklmnopqrstuvwxyz"
+    if shift == 0:
+        return message
+    for el in message:
+        if el == " ":
+            encoded_message += " "
+        else:
+            shifted_letter_num = alphabet.find(el)
+            cipher_to_letter_num = (shifted_letter_num + shift) % 26
+            encoded_message += alphabet[cipher_to_letter_num]
+
+    return encoded_message
+
+
+if __name__ == '__main__':
+    print(encode("i like turtles", 6))  # -> o roqk zaxzrky
+    print(encode("o roqk zaxzrky", 20))  # -> i like turtles
+    print(encode("example", 1))  # -> fybnqmf
+    print(encode("don't change", 0))  # -> don't change
+    print(encode('the quick brown fox jumps over the lazy dog.', 7))  # -> aol xbpjr iyvdu mve qbtwz vcly aol shgf kvn.
