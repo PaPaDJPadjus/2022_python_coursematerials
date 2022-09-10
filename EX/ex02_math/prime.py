7d45e762fd23a0589c79d01184272e6d82951412
Patrick Padjus <papadj@ttu.ee>
2022-09-10 18:27:24 +0300

    Prime nrs

--- EX/ex02_math/prime.py
+++ EX/ex02_math/prime.py
@@ -8,11 +8,11 @@ def is_prime_number(number: int):
     Prime number is a natural number which is only divisible by 1 and itself. 0 and 1 are not prime numbers.
 
     Conditions:
-    1. If number is a prime number then return boolean True
-    2. If number is not a prime number then return boolean False
+    1. If number is a prime number, then return boolean True
+    2. If number is not a prime number, then return boolean False
 
-    :param number: the number to check.
-    :return: boolean True if number is a prime number or False if number is not a prime number.
+    param number: the number to check.
+    return: boolean True if number is a prime number or False if number is not a prime number.
     """
     sum_total = 0
     for i in range(2, number + 1):
