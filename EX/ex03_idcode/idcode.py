c8638c2c82286c437a9336d61b3fe96c1e016ede
Patrick Padjus <papadj@ttu.ee>
2022-09-18 12:50:23 +0300

    Final version?

--- EX/ex03_idcode/idcode.py
+++ EX/ex03_idcode/idcode.py
@@ -92,17 +92,75 @@ def the_first_control_number_algorithm(text: str) -> str:
         return "Incorrect ID code!"
 
 
-if __name__ == '__main__':
-    print("\nFind ID code:")
-    print(find_id_code(""))  # -> "Not enough numbers!"
-    print(find_id_code("123456789123456789"))  # -> "Too many numbers!"
-    print(find_id_code("ID code is: 49403136526"))  # -> "49403136526"
-    print(find_id_code("efs4  9   #4aw0h 3r 1a36g5j2!!6-"))  # -> "49403136526"
+def is_valid_year_number(year_number: int):
+    """Check if given value is correct for year number in ID code."""
+    if year_number < 00:
+        return False
+    if year_number > 99:
+        return False
+    else:
+        return True
 
-    print(the_first_control_number_algorithm(""))  # -> "Incorrect ID code!"
-    print(the_first_control_number_algorithm("12345678912"))  # -> "Incorrect ID code!"
-    print(the_first_control_number_algorithm("ID code is: 49403136526"))  # -> "49403136526"
-    print(the_first_control_number_algorithm("efs4  9   #4aw0h 3r 1a36g5j2!!6-"))  # -> "49403136526"
-    print(the_first_control_number_algorithm("50412057633"))
-    print(the_first_control_number_algorithm("Peeter's ID is 50205160274"))  # -> "Needs
-    # the second algorithm!"
+
+def is_valid_month_number(month_number: int):
+    """Check if given value is correct for month number in ID code."""
+    if month_number < 00:
+        return False
+    if month_number > 12:
+        return False
+    else:
+        return True
+
+
+def is_valid_birth_number(birth_number: int) -> bool:
+    """Check if given value is correct for birth number in ID code."""
+    if birth_number <= 0:
+        return False
+    if birth_number > 999:
+        return False
+    else:
+        return True
+
+
+def is_valid_gender_number(first_nr: int):
+    """Check if gender number is valid."""
+    if first_nr == 0 or first_nr > 6:
+        return False
+    else:
+        return True
+
+
+def get_gender(first_nr: int):
+    """Check if is male or female."""
+    if first_nr == 0 or first_nr > 6:
+        return False
+    if first_nr == 1 or first_nr == 3 or first_nr == 5:
+        return "Male"
+    if first_nr == 2 or first_nr == 4 or first_nr == 6:
+        return "Female"
+
+
+if __name__ == '__main__':
+    print("\nGender number:")
+    for i in range(9):
+        print(f"{i} {is_valid_gender_number(i)}")
+        # 0 -> False
+        # 1...6 -> True
+        # 7...8 -> False
+
+    print("\nGet gender:")
+    print(get_gender(2))  # -> "female"
+    print(get_gender(5))  # -> "male"
+
+    print("\nYear number:")
+    print(is_valid_year_number(100))  # -> False
+    print(is_valid_year_number(50))  # -> True
+
+    print("\nMonth number:")
+    print(is_valid_month_number(2))  # -> True
+    print(is_valid_month_number(15))  # -> False
+
+    print("\nBorn order number:")
+    print(is_valid_birth_number(0))  # -> False
+    print(is_valid_birth_number(1))  # -> True
+    print(is_valid_birth_number(850))  # -> True
