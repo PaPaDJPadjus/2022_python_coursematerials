a8e4f13dcce0b9e22ca450a3e2b14142d2549a30
Patrick Padjus <papadj@ttu.ee>
2022-09-24 10:13:53 +0300

    aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa

---
+++ EX/ex04_lists/lists.py
@@ -0,0 +1,47 @@
+"""Car inventory."""
+
+
+def list_of_cars(all_cars: str) -> list:
+    """
+    Return list of cars.
+
+    The input string contains of car makes and models, separated by comma.
+    Both the make and the model do not contain spaces (both are one word).
+
+    "Audi A4,Skoda Superb,Audi A4" => ["Audi A4", "Skoda Superb", "Audi A4"]
+    """
+    if all_cars == "":
+        return []
+    cars_list = all_cars.split(",")
+    return [cars_list]
+
+
+def car_makes(all_cars: str) -> list:
+    """
+    Return list of unique car makes.
+
+    The order of the elements should be the same as in the input string (first appearance).
+
+    "Audi A4,Skoda Superb,Audi A4" => ["Audi", "Skoda"]
+    """
+    list_of_cars(all_cars)
+    car_name = []
+    i = 0
+    for el in all_cars:
+        if i == 1:
+            i = 0
+        if i == 0:
+            car_name.append(el)
+            i += 1
+    return [car_name]
+
+
+def car_models(all_cars: str) -> list:
+    """
+    Return list of unique car models.
+
+    The order of the elements should be the same as in the input string (first appearance).
+
+    "Audi A4,Skoda Superb,Audi A4,Audi A6" => ["A4", "Superb", "A6"]
+    """
+    return []
