74d44c7555b964bd08e42d8ee4bae33516184501
Patrick Padjus <papadj@ttu.ee>
2022-10-16 22:45:18 +0300

    aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa

--- EX/ex07_files/file_handling.py
+++ EX/ex07_files/file_handling.py
@@ -215,9 +215,8 @@ def merge_dates_and_towns_into_csv(dates_filename: str, towns_filename: str, csv
                 list_list = []
 
     with open(csv_output_filename, 'w') as csv_file:
-        csv_writer = csv.writer(csv_file, delimiter=",")
+        csv_writer = csv.writer(csv_file)
         csv_writer.writerow(["name", "town", "date"])
         for row in list_of_stuff:
             csv_writer.writerow(row)
-    print(list_of_stuff)
     return
