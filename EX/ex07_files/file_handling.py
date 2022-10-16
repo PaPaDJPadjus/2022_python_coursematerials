e950610a8006c92a1449583c04ea5228a4e0f177
Patrick Padjus <papadj@ttu.ee>
2022-10-16 21:00:08 +0300

    aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa

--- EX/ex07_files/file_handling.py
+++ EX/ex07_files/file_handling.py
@@ -68,7 +68,8 @@ def read_csv_file(filename: str) -> list:
     with open(filename) as csv_file:
         csv_reader = csv.reader(csv_file, delimiter=',')
         for row in csv_reader:
-            return [row]
+            lists.append([row])
+    return lists
 
 
 
