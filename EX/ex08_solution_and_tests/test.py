0de553ab0f2718861d4b5ef8270ddfc3a459935d
Patrick Padjus <papadj@ttu.ee>
2022-10-24 13:32:49 +0300

    aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa

--- EX/ex08_solution_and_tests/test.py
+++ EX/ex08_solution_and_tests/test.py
@@ -2,7 +2,7 @@ import pytest
 from solution import students_study
 
 
-def test_students_study_night_always_true():
+def test_students_study_evening_always_true():
     """Test nightly study session times."""
     assert students_study(18, True) is True
     assert students_study(18, False) is True
@@ -10,3 +10,9 @@ def test_students_study_night_always_true():
     assert students_study(24, False) is True
 
 
+def test_students_study_night_always_false():
+    """Test if student is sleaping."""
+    assert students_study(1, True) is False
+    assert students_study(1, False) is False
+    assert students_study(4, True) is False
+    assert students_study(4, False) is False
