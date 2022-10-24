960170d2edd0e35bfda28963817883efff0ce8c7
Patrick Padjus <papadj@ttu.ee>
2022-10-24 13:42:35 +0300

    aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa

--- EX/ex08_solution_and_tests/test.py
+++ EX/ex08_solution_and_tests/test.py
@@ -1,6 +1,7 @@
 import pytest
 from solution import students_study
-
+from solution import lottery
+from solution import fruit_order
 
 def test_students_study_evening_always_true():
     """Test nightly study session times."""
@@ -25,3 +26,27 @@ def test_students_study_day_false():
     assert students_study(5, True) is True
     assert students_study(5, False) is False
 
+
+def test_lottery_jackpot():
+    """Test jackpot aka all 5."""
+    assert lottery(5, 5, 5) == 10
+
+
+def test_lottery_same_number_not_5():
+    """Test if numbers are equal but not 5."""
+    assert lottery(1, 1, 1) == 5
+    assert lottery(4, 4, 4) == 5
+    assert lottery(8, 8, 8) == 5
+
+
+def test_lottery_all_different():
+    """Test if numbers are different for mini prize."""
+    assert lottery(1, 2, 3) == 1
+    assert lottery(4, 1, 9) == 1
+    assert lottery(0, 10, 1) == 1
+
+
+def test_lottery_a_is_b_or_c_not_both():
+    """Test if a is equal to b or c."""
+    assert lottery(1, 1, 3) == 0
+    assert lottery(0, 4, 0) == 0
