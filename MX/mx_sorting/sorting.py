e21a69f0e20067ae6ccc24374ae9698a06a9af61
Patrick Padjus <papadj@ttu.ee>
2022-11-08 14:01:36 +0200

    aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa

--- MX/mx_sorting/sorting.py
+++ MX/mx_sorting/sorting.py
@@ -137,10 +137,8 @@ def sort_people_by_popularity_of_name(people: list[Person]) -> list:
     :param people: Input list of people (Objects of the Person class).
     :return: Sorted list of people.
     """
-    names = []
-    for person in people:
-        names.append(person.name)
-    sorted_list = sorted(people, key=lambda x: (-names.count(x.name), x.name))
+    mapped_list = list(map(lambda x: x.name, people))
+    sorted_list = sorted(people, key=lambda x: (-mapped_list.count(x.name), x.name))
     return sorted_list
 
 
