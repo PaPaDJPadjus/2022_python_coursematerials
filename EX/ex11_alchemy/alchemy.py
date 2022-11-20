f63e62eccd08560a309b5e7b476ecea05e46ae3a
Patrick Padjus <papadj@ttu.ee>
2022-11-20 16:02:41 +0200

    aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa

--- EX/ex11_alchemy/alchemy.py
+++ EX/ex11_alchemy/alchemy.py
@@ -148,6 +148,8 @@ class AlchemicalRecipes:
         """
         if first_component_name == second_component_name:
             raise DuplicateRecipeNamesException
+        if first_component_name == product_name or second_component_name == product_name:
+            raise DuplicateRecipeNamesException
         if first_component_name != second_component_name != product_name:
             if product_name not in self.dict_recipe.keys():
                 new_recipe = [first_component_name, second_component_name]
