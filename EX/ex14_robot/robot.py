988f3d747f4d636c094a22d99c142f11391aa3b5
Patrick Padjus <papadj@ttu.ee>
2022-12-06 00:23:32 +0200

    aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa

--- EX/ex14_robot/robot.py
+++ EX/ex14_robot/robot.py
@@ -13,3 +13,20 @@ def test_run(robot: FollowerBot()):
     robot.sleep(2)
     robot.set_wheels_speed(0)
     robot.done()
+
+
+def drive_to_line(robot: FollowerBot):
+    """
+    Drive the robot until it meets a perpendicular black line, then drive forward 25cm.
+
+    There are 100 pixels in a meter.
+
+    :param FollowerBot robot: instance of the robot that you need to make move
+    """
+    robot.set_wheels_speed(25)
+    robot.sleep(1)
+    robot.set_wheels_speed(0)
+    for value in robot.get_line_sensors():
+        if value == 0:
+            robot.done()
+        drive_to_line(robot)
