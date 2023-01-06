8c4d3c29369c887bc5ef032f896fa5aa2b9e5576
Patrick Padjus <papadj@ttu.ee>
2023-01-06 21:49:51 +0200

    aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa

--- EX/ex15_santas_workshop/attempt_two.py
+++ EX/ex15_santas_workshop/attempt_two.py
@@ -10,13 +10,14 @@ class NeededLists:
         """Lists that I need."""
         self.all_names = []
         self.children = []
-        self.naughty_kids_in_tuples = []
+        self.naughty_kids_in_tuples = []   # Kasutan, et hiljem kontrollida, et sama laps pole naughty ja nice
+        self.gifts_list = []               # List kingitustest (ja söest) mida lastele anda
+        self.nice_kid_counter = 0      # Kui jääb nulliks, skipib mõne funktsiooni ja aitab
 
 
-class Child(NeededLists):
-    """Child class, that holds its info."""
+class Child:
+    """Child class, that holds its name, place, behaviour and wish list."""
     def __init__(self):
-        super().__init__()
         self.name = ""
         self.country = ""
         self.behaviour = ""
@@ -25,6 +26,14 @@ class Child(NeededLists):
     def __repr__(self):
         """Show me urself."""
         return f"{self.behaviour} {self.name}, from {self.country} wants {self.wishes}"
+
+
+class GiftDatabase(NeededLists):
+    """Store info bout gifts."""
+    def __init__(self):
+        super().__init__()
+        self.url = "https://cs.ttu.ee/services/xmas/gift?name="
+        self.gifts = {}
 
     def info_from_csv_files(self, nice_list: str, naughty_list: str, wish_list: str):
         """Get the names from the file and link them with the wanted gifts."""
@@ -52,29 +61,21 @@ class Child(NeededLists):
                 child_from_list.behaviour = "Nice"
                 self.children.append(child_from_list)
                 self.all_names.append(child_from_list.name)
+                self.nice_kid_counter += 1
 
-        with open(wish_list) as csv_file:
-            csv_reader = csv.reader(csv_file, delimiter=",")
-            for row in csv_reader:
+        if self.nice_kid_counter != 0:
+            with open(wish_list) as csv_file:
+                csv_reader = csv.reader(csv_file, delimiter=",")
+                for row in csv_reader:
 
-                for child in self.children:
-                    if row[0] == child.name:
-                        for gift in row[1::]:
-                            child.wishes.append(gift)
-                    else:
-                        continue
+                    for child in self.children:
+                        if row[0] == child.name:
+                            for gift in row[1::]:
+                                child.wishes.append(gift)
+                        else:
+                            continue
 
-        return [self.children]
-
-
-class GiftDatabase(NeededLists):
-    """Store info bout gifts."""
-
-    def __init__(self):
-        super().__init__()
-        self.url = "https://cs.ttu.ee/services/xmas/gift?name="
-        self.gifts = {}
-        self.gifts_list = []
+        return self.children
 
     def __get_gift_from_api(self, name):
         """Api request."""
@@ -86,9 +87,8 @@ class GiftDatabase(NeededLists):
 
     def get_gift(self, name):
         """Get the gift """
-        if name in self.gifts:
+        if name in self.gifts.keys():
             return self.gifts[name]
-
         return self.__get_gift_from_api(name)
 
     def get_needed_gifts_for_children(self):
@@ -99,6 +99,7 @@ class GiftDatabase(NeededLists):
 
             if child.behaviour == "Nice":
                 for gift in child.wishes:
+                    self.gifts[gift] = self.get_gift(gift)
                     self.gifts_list.append(gift)
 
             else:
@@ -109,6 +110,8 @@ class GiftDatabase(NeededLists):
 
 
 if __name__ == '__main__':
+    lists = NeededLists()
+    gifts = GiftDatabase()
+    print(gifts.info_from_csv_files("test_nice_one.csv", "test_naughty_one.csv", "test_wish_list_one.csv"))
+    print(gifts.get_needed_gifts_for_children())
 
-    print(Child().info_from_csv_files("nice_list_same_name.csv", "naughty_list_same_name.csv", "wish_list_same_names.csv"))
-    print(GiftDatabase().get_needed_gifts_for_children())
--- EX/ex15_santas_workshop/ex15_tests.py
+++ EX/ex15_santas_workshop/ex15_tests.py
@@ -7,17 +7,55 @@ from attempt_two import GiftDatabase
 
 def test_if_reads_info_from_files_singular_name_in_each_list():
     """Feed files to code and see, if it reads the files into the correct lists."""
-    result = Child().info_from_csv_files("test_nice_one.csv", "test_naughty_one.csv", "test_wish_list_one.csv")
-    assert result[0][0].name == "Kati"
-    assert result[0][1].name == "Mati"
-    assert result[0][1].wishes == ['Cars', ' Money']
-    assert result[0][0].wishes == ['Dolls', ' Money']
+    result = GiftDatabase().info_from_csv_files("test_nice_one.csv", "test_naughty_one.csv", "test_wish_list_one.csv")
+    assert result[0].name == "Kati"
+    assert result[1].name == "Mati"
+    assert result[1].wishes == ['Cars', 'Money']
+    assert result[0].wishes == ['Dolls', 'Money']
 
 
 def test_if_same_person_on_both_lists_only_represent_naughty():
     """If someone is on both lists, remove them from the nice list and make them naughty."""
-    result = Child().info_from_csv_files("nice_list_same_name.csv", "naughty_list_same_name.csv", "wish_list_same_names.csv")
-    assert result[0][0].behaviour == "Naughty"
-    assert result[0][1].behaviour == "Naughty"
-    assert result[0][2].behaviour == "Naughty"
-    assert result[0][3].behaviour == "Nice"
+    result = GiftDatabase().info_from_csv_files("nice_list_same_name.csv", "naughty_list_same_name.csv", "wish_list_same_names.csv")
+    assert result[0].behaviour == "Naughty"
+    assert result[1].behaviour == "Naughty"
+    assert result[2].behaviour == "Naughty"
+    assert result[3].behaviour == "Nice"
+
+
+def test_if_gets_gift_info():
+    """Test getting gift info if it doesn't have it."""
+    gifts = GiftDatabase()
+    result_one = gifts.get_gift("Small watering can")
+    result_two = gifts.get_gift("Pink tricycle")
+    assert result_one == {'gift': 'Small watering can', 'material_cost': 7, 'production_time': 2, 'weight_in_grams': 540}
+    assert result_two == {'gift': 'Pink tricycle', 'material_cost': 159, 'production_time': 8, 'weight_in_grams': 5400}
+
+
+def test_incorrect_wish():
+    """If the item doesn't exist, should fail."""
+    gifts = GiftDatabase()
+    result = gifts.get_gift("Girlfriend")
+    assert result == {'message': 'Gift not found! Did you forget to supply the name of the gift as a query parameter?'}
+
+
+def test_when_nice_list_empty():
+    """Test code for circumstance of no nice list."""
+    gifts = GiftDatabase()
+    result = gifts.info_from_csv_files("empty.csv", "naughty_list_same_name.csv", "wish_list_same_names.csv")
+    assert len(result) == 3
+
+
+def test_when_naughty_list_empty():
+    """Test code for circumstance of no naughty list."""
+    gifts = GiftDatabase()
+    result = gifts.info_from_csv_files("nice_list_same_name.csv", "empty.csv", "wish_list_same_names.csv")
+    assert len(result) == 2
+
+
+def test_when_wish_list_empty():
+    """Test code for circumstance of no wish list."""
+    gifts = GiftDatabase()
+    gifts.info_from_csv_files("nice_list_same_name.csv", "naughty_list_same_name.csv", "empty.csv")
+    result = gifts.get_needed_gifts_for_children()
+    assert result == ['3 pieces of coal']
